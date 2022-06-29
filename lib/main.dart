ea2fa137b3a56296e12e113d99e82e7543318418
RamonTadeuGoncalves <ramontadeugoncalves@hotmail.com>
2022-06-29 15:16:27 -0300

    Created card for new transactions, and also formated datetime visualization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
-
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-main() => runApp(ExpensesApp());
+void main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   @override
@@ -13,17 +13,17 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
         id: 't1',
-        title: 'New running shoes',
+        title: 'Novo tenis de corrida',
         value: 310.76,
         date: DateTime.now()),
     Transaction(
-        id: 't2',
-        title: 'Electricity bill',
-        value: 211.30,
-        date: DateTime.now())
+        id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now())
   ];
 
   @override
@@ -33,7 +33,6 @@ class MyHomePage extends StatelessWidget {
           title: Text('Personal Expenses'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -55,7 +54,7 @@ class MyHomePage extends StatelessWidget {
                             border: Border.all(color: Colors.purple, width: 2)),
                         padding: EdgeInsets.all(10),
                         child: Text(
-                          tr.value.toString(),
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 20,
@@ -71,7 +70,7 @@ class MyHomePage extends StatelessWidget {
                                 fontWeight: FontWeight.bold, fontSize: 16),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: TextStyle(color: Colors.grey),
                           )
                         ],
@@ -80,6 +79,40 @@ class MyHomePage extends StatelessWidget {
                   ),
                 );
               }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(labelText: 'Titulo'),
+                    ),
+                    TextField(
+                      controller: valueController,
+                      decoration: InputDecoration(labelText: 'Valor R\$'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              print(titleController.text);
+                              print(valueController.text);
+                            },
+                            child: Text(
+                              'Nova Transação',
+                              style: TextStyle(
+                                  color: Colors.purple,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ));
