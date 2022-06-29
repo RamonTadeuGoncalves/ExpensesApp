ee6f79029b48b3e5a74adf220a29e9b703b8f00e
RamonTadeuGoncalves <ramontadeugoncalves@hotmail.com>
2022-06-29 11:07:17 -0300

    Created models folder and the file transaction.dart. Also created function to display the result on the screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+import 'dart:math';
 
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: MyHomePage());
@@ -13,29 +14,75 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'New running shoes',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Electricity bill',
+        value: 211.30,
+        date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Personal Expenses'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            child: Card(
-              child: Text('Graphic'),
-              elevation: 5,
-              color: Colors.blue,
+        appBar: AppBar(
+          title: Text('Personal Expenses'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                child: Text('Graphic'),
+                elevation: 5,
+                color: Colors.blue,
+              ),
             ),
-          ),
-          Card(
-            child: Text('Transaction List'),
-          )
-        ],
-      )
-    );
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tr.value.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text(
+                            tr.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
